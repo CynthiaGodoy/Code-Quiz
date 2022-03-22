be04646dd2f539510fdfbac56c7ae3b1fcd8e893
Cynthia Godoy <99221047+CynthiaGodoy@users.noreply.github.com>
2022-03-22 15:14:33 -0600

    Questions display but still not working
    Timer and Choices still having issues

--- script.js
+++ script.js
@@ -1,53 +1,28 @@
 // CREATES ARRAY WITH QUESTIONS, CHOICES, & ANSWERS
 var questions = [
   {
-    title: "Which keyword is used to declare a variable in JS?",
-    choices: [
-      "Var",
-      "Dim",
-      "String",
-      "None of the Above"
-    ],
+    title: "Which keyword is used to declare a variable in JavaScript?",
+    choices: ["Var", "Dim", "String", "None of the Above"],
     answer: "Var"
   },
   {
     title: "Which of the following is a Front-end Framework?",
-    choices: [
-      "Express.js",
-      "Bootstrap",
-      "Cake.php",
-      "Django"
-    ],
+    choices: ["Express.js", "Bootstrap", "Cake.php","Django"],
     answer: "Bootstrap"
   },
   {
-    question: "Inside which HTML element do we put the JavaScript?",
-    choices: [
-      "javascript",
-      "js",
-      "script",
-      "scripting"
-    ],
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["javascript", "js", "script", "scripting"],
     answer: "script"
   },
   {
-    question: "Where is the correct place to insert a JavaScript?",
-    choices: [
-      "The head section",
-      "The body section",
-      "The head and body section",
-      "Neither head nor body section"
-    ],
+    title: "Where is the correct place to insert a JavaScript?",
+    choices: ["The head section", "The body section", "The head and body section", "Neither head nor body section"],
     answer: "The head and body section"
   },
   {
-    question: "How do you write a comment in JavaScript",
-    choices: [
-      "/*This is a comment*/",
-      "//This is a comment",
-      "<--This is a comment-->",
-      "This is a comment"
-    ],
+    title: "How do you write a comment in JavaScript",
+    choices: ["/*This is a comment*/", "//This is a comment", "<--This is a comment-->", "This is a comment"],
     answer: "//This is a comment"
   },
 ];
@@ -57,11 +32,11 @@ var score = 0;
 var questionsIndex = 0;
 
 var currentTime = document.querySelector("currentTime");
-var timer = document.querySelector("#startTime")
-var questionsDiv = document.querySelector("#questionDiv")
-var gameBox = document.querySelector("#game-box")
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
 
-var secondsLeft = 75; //SECONDS LEFT
+let secondsLeft = 75; //SECONDS LEFT
 var holdInterval = 0; //INTERVAL TIME
 var penalty = 10; //PENALTY TIME
 var ulCreate = document.createElement("ul"); //CREATES NEW ELEMENT
@@ -69,7 +44,7 @@ var ulCreate = document.createElement("ul"); //CREATES NEW ELEMENT
 timer.addEventListener("click", function () { //TRIGGERS TIMER ON BUTTON
   if (holdInterval === 0) { //CHECKING ZERO
     holdInterval = setInterval(function () {
-    secondsLeft--;
+    secondsLeft --;
     currentTime.textContent = "Time: " + secondsLeft;
 
   if (secondsLeft <= 0) {
@@ -79,16 +54,17 @@ timer.addEventListener("click", function () { //TRIGGERS TIMER ON BUTTON
   }
   }, 1000); //PAUSES PROGRAM FOR THE AMOUNT OF TIME (in milliseconds) AS SPECIFIED IN THE PARAMETER.
 }
-  render(questionIndex);
+  render(questionsIndex);
 });
 
 //RENDERS QUESTIONS & CHOICES TO PAGE
-questionsDiv.innerHTML = ""; //CLEARS EXISTING DATA
-ulCreate.innerHTML = "";
+function render(questionsIndex) {
+  questionsDiv.innerHTML = ""; //CLEARS EXISTING DATA
+  ulCreate.innerHTML = "";
 
 for(var i = 0; i <questions.length; i++) { //LOOPS THROUGH ARRAY
-  var userQuestion = questions[questionIndex].title; //QUESTIONS
-  var userChoices = questions[questionIndex].choices;
+  var userQuestion = questions[questionsIndex].title; //QUESTIONS
+  var userChoices = questions[questionsIndex].choices;
   questionsDiv.textContent = userQuestion;
 }
 
@@ -99,6 +75,7 @@ userChoices.forEach(function (newItem) { //CHOICES
   ulCreate.appendChild(listItem);
   listItem.addEventListener("click", (compare));
 })
+}
 
 function compare(event) {
   var element = event.target;
@@ -108,18 +85,19 @@ function compare(event) {
     var createDiv = document.createElement("div");
     createDiv.setAttribute("id", "createDiv");
 
-    if (element.textContent == question[questionIndex].answer) {
+    if (element.textContent == questions[questionsIndex].answer) {
       score++;
-      createDiv.textContent = "Correct! The answer is: " + questions[questionIndex].answer; //USER IS CORRECT
+      createDiv.textContent = "Correct! The answer is: " + questions[questionsIndex].answer; //USER IS CORRECT
   } else {
     secondsLeft = secondsLeft - penalty;
-    createDiv.textContent = "Wrong! The correct answer is: " + questions[questionIndex].answer; //USER IS WRONG
+    createDiv.textContent = "Wrong! The correct answer is: " + questions[questionsIndex].answer; //USER IS WRONG
   }
 }
 
-if (questionIndex >= questions.length) { //QUESTION INDEX DETERMINES NUMBER USER IS ON
+questionsIndex++;
+if (questionsIndex >= questions.length) { //QUESTION INDEX DETERMINES NUMBER USER IS ON
     allDone(); 
-    createDiv.textContent = "End of Quiz!" + "" + "You got " + score + "/" + questions.length;
+    createDiv.textContent = "End of Quiz!" + "" + "You got " + score + "/" + questions.length + "Correct!";
 } else {
   render(questionIndex);
 }
@@ -149,27 +127,27 @@ function allDone() { //ALLDONE
     questionsDiv.appendChild(createP2);
   }
 
-  var createLabel = document.createElement("input");
+  var createLabel = document.createElement("label"); //LABEL
   createLabel.setAttribute("id", "createLabel");
   createLabel.textContent = "Enter your initials: ";
 
   questionsDiv.appendChild(createLabel);
 
-  var createInput = document.createElement("button");
+  var createInput = document.createElement("input"); //INPUT
   createInput.setAttribute("type", "text");
-  createInput.setAttribute("id", "Submit");
-  createInput.textContent = "Submit";
+  createInput.setAttribute("id", "initials");
+  createInput.textContent = "";
 
   questionsDiv.appendChild(createInput);
 
-  var createSubmit = document.createElement("button");
-  createSubmit.setAttribute("type", "text");
+  var createSubmit = document.createElement("button"); //SUBMIT
+  createSubmit.setAttribute("type", "submit");
   createSubmit.setAttribute("id", "Submit");
   createSubmit.textContent = "Submit";
 
-  questionsDiv.appendChild(createInput);
+  questionsDiv.appendChild(createSubmit);
 
-  createSubmit.addEventListener("click", function() {
+  createSubmit.addEventListener("click", function() { //EVENT LISTENER TO CAPTURE INITIALS & SCORE & LOCAL STORAGE
     var initials = createInput.value;
 
     if (initials ===null) {
@@ -181,16 +159,16 @@ function allDone() { //ALLDONE
       }
         console.log(finalScore);
         var allScores = localStorage.getItem("allScores");
-        if (allScores ===null) {
+        if (allScores === null) {
         allScores = [];
-        }else {
+        } else {
           allScores = JSON.parse(allScores);
         }
          allScores.push(finalScore);
          var newScore = JSON.stringify(allScores);
          localStorage.setItem("allScores", newScore);
 
-         window.location.replace("./score.html")
+         window.location.replace("./score.html") //SENDS TO HTML
       }
     });
 }
